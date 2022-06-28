d5695954dfa45a5a0e03c84699d30073f0f9c1fc
EdsonSantosCode <dev.escode@hotmail.com>
2022-06-28 20:46:17 -0300

    Subindo Projeto.

---
+++ src/ESCode.UI.Site/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESCode.UI.Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
